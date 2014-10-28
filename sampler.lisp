be52d178e46766c47f1658a953f12f88d7c8f800
john.maraist <john.maraist@gmail.com>
2014-10-28 04:24:21 +0000

    Generate an org-mode index of what's written out.

--- sampler.lisp
+++ sampler.lisp
@@ -53,7 +53,22 @@
         (symbol-name sym) (constantp sym))
       (when doc-string (format stream "~%~%  ~a~%~%" doc-string)))))
 
-(defun write-symbol-files (symbol directory-path &key always-disambiguate)
+(defun set-index-usage (index-acc symbol usage path)
+  "The =index-acc= hash table builds up path information for an index."
+
+  ;; Pull the hash table for the SYMBOL, creating and caching one if
+  ;; needed.
+  (let ((symbol-hash (gethash symbol index-acc)))
+    (unless symbol-hash
+      (setf symbol-hash (make-hash-table :test 'eq)
+            (gethash symbol index-acc) symbol-hash))
+
+    ;; Store the path for this use of the symbol
+    (setf (gethash usage symbol-hash) path))
+  (values))
+
+(defun write-symbol-files (symbol directory-path
+                           &key index-acc always-disambiguate)
   "Writes Org-mode files documenting the uses of the given =symbol=."
   (let* ((use-name (string-downcase (symbol-name symbol)))
          (is-fn (fboundp symbol))
@@ -69,24 +84,31 @@
             (setf (aref use-name i) #\_)))
 
     (when is-fn
-      (write-fn-file symbol (merge-pathnames
-                             (concatenate 'string
-                               use-name (if disambiguate "__fn" "") ".org")
-                             directory-path)))
-    (when is-type
-      (write-type-file symbol
-                       (merge-pathnames
-                        (concatenate 'string
-                          use-name (if disambiguate "__type" "") ".org")
-                        directory-path)))
-    (when is-variable
-      (write-variable-file symbol
-                       (merge-pathnames
-                        (concatenate 'string
-                          use-name (if disambiguate "__variable" "") ".org")
-                        directory-path)))))
+      (let ((path (merge-pathnames (concatenate 'string
+                                     use-name (if disambiguate "__fn" "")
+                                     ".org")
+                                   directory-path)))
+        (write-fn-file symbol path)
+        (when index-acc (set-index-usage index-acc symbol :fn path))))
 
-(defun write-package-files (package &key all system path)
+    (when is-type
+      (let ((path (merge-pathnames
+                   (concatenate 'string
+                     use-name (if disambiguate "__type" "") ".org")
+                   directory-path)))
+        (write-type-file symbol path)
+        (when index-acc (set-index-usage index-acc symbol :type path))))
+
+    (when is-variable
+      (let ((path (merge-pathnames
+                   (concatenate 'string
+                     use-name (if disambiguate "__variable" "") ".org")
+                   directory-path)))
+        (write-variable-file symbol path)
+        (when index-acc (set-index-usage index-acc symbol :variable path))))))
+
+(defun write-package-files (package &key all system path index
+                                      (index-acc (make-hash-table :test 'eq)))
   "Documents a package by writing an Org file for each defined symbol.
 - The =package= argument should be a package specifier.
 - If the =all= keyword argument is given and is non-nil, then all symbols in
@@ -101,13 +123,18 @@
   (ensure-directories-exist path)
   (cond (all (do-symbols (sym package)
                (when (eq (symbol-package sym) (find-package package))
-                 (write-symbol-files sym path))))
+                 (write-symbol-files sym path :index-acc index-acc))))
         (t (do-external-symbols (sym package)
-             (write-symbol-files sym path)))))
+             (write-symbol-files sym path :index-acc index-acc))))
+  (when index
+    (format t "Writing index for package.~%")
+    (write-index-org index-acc path index)))
 
 (defun write-packages (packages &key default-all default-system
                                   (default-path "./")
-                                  (package-extension t) (extension-downcase t))
+                                  (package-extension t) (extension-downcase t)
+                                  index (index-system default-system)
+                                  (index-acc (make-hash-table :test 'eq)))
   "Document several packages by making a call to =write-package-files= for each.
 The =packages= argument is a list giving a specification of the packages to be
 documented.  Each element can be either a package designator or a list whose
@@ -119,20 +146,47 @@ default arguments for the calls to =write-package-files=.
 If =package-extension= is non-nil (its default is =t=), then whenever a package
 spec does not give an explicit path, it should use a subdirectory of the default
 path whose name is taken from the package.  If =extension-downcase= is non-nil
-(its default is =t=), then the package name is converted to lower-case for this
+\(its default is =t=), then the package name is converted to lower-case for this
 extension."
   (iter (for package-spec in packages)
         (when (symbolp package-spec)
           (setf package-spec (list package-spec)))
         (destructuring-bind (package &key (all default-all)
                                           (system default-system)
-                                          (path default-path path-supp-p))
+                                          (path default-path path-supp-p)
+                                          (subdir "./"))
             package-spec
-          (unless path-supp-p
-            (when package-extension
-              (let ((extension (package-name package)))
-                (when extension-downcase
-                  (setf extension (string-downcase extension)))
-                (setf path (merge-pathnames (concatenate 'string extension "/")
-                                            path)))))
-          (write-package-files package :all all :system system :path path))))
+          (cond
+            (path-supp-p
+             (when package-extension
+               (let ((extension (package-name package)))
+                 (when extension-downcase
+                   (setf extension (string-downcase extension)))
+                 (setf path (merge-pathnames (concatenate 'string extension "/")
+                                             path)))))
+            (t (setf path (merge-pathnames subdir path))))
+          (write-package-files package :all all :system system :path path
+                               :index-acc index-acc)))
+  (when index
+    (format t "Writing index for packages.~%")
+    (write-index-org index-acc
+                     (asdf:system-relative-pathname index-system default-path)
+                     index))
+  (values))
+
+(defun write-index-org (index-acc directory-path index-file-name)
+  (unless (stringp index-file-name) (setf index-file-name "index"))
+  (let ((path (merge-pathnames (concatenate 'string index-file-name ".org")
+                               directory-path))
+        (names (sort (iter (for (key val) in-hashtable index-acc)
+                           (declare (ignore val))
+                           (collect key))
+                     #'string< :key #'symbol-name)))
+    (with-open-file (out path :direction :output :if-exists :supersede)
+      (iter (for name in names)
+            (format out "- ~a" name)
+            (iter (for (usage path) in-hashtable (gethash name index-acc))
+                  (for sep initially ":" then ",")
+                  (format out "~a [[file:~a][~a]]" sep path usage))
+            (format out ".~%"))))
+  (values))
